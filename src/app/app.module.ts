ac96fce9a233826b8f55110e16714189dbd0def4
Muteman30 <jameshyde30@gmail.com>
2020-09-01 20:53:14 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InmateListComponent } from './inmate-list/inmate-list.component';
+import { InmateComponent } from './inmate-list/inmate/inmate.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InmateListComponent,
+    InmateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/inmate-list/inmate-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inmate-list',
+  templateUrl: './inmate-list.component.html',
+  styleUrls: ['./inmate-list.component.css']
+})
+export class InmateListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
