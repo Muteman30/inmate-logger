09a41ffc5780c64fb4257455eefbf51da436bf7f
Muteman30 <jameshyde30@gmail.com>
2020-09-06 20:43:40 +0100

    added the filtering

--- src/app/inmate-list/inmate-list.component.ts
+++ src/app/inmate-list/inmate-list.component.ts
@@ -12,7 +12,8 @@ export class InmateListComponent implements OnInit {
 
   sortOpts={
     prop:'name',
-    asc:true
+    asc:true,
+    filterVal:''
   }
 
   constructor() {
@@ -33,24 +34,42 @@ export class InmateListComponent implements OnInit {
     this.chosenInmate.emit(newInmate);
   }
 
-  onResortInmates(propName:string){
+  onResortInmates(event, propName:string){
     if(this.sortOpts.prop == propName){
       this.sortOpts.asc = !this.sortOpts.asc;
     } else {
       this.sortOpts.asc = true;
     }
 
+    var headerCols = event.target.parentElement.children;
+    for(let header of headerCols){
+      header.classList.remove('asc', 'desc', 'sorting-col');
+    };
+
+    event.target.classList.add(this.sortOpts.asc?'asc' : 'desc');
+    
     this.sortOpts.prop = propName;
+
+    event.target.classList.add('sorting-col');
+  }
+
+  onFilterInmates(event){
+    this.sortOpts.filterVal = event.target.value;
+
   }
 
   sortInmates(){  
-    return this.inmates.sort((a:Inmate, b:Inmate):number=>{
+    return this.inmates.filter((inmate:Inmate)=>{
+      return inmate.name.indexOf(this.sortOpts.filterVal) > -1 ||
+      inmate.id.indexOf(this.sortOpts.filterVal) > -1 ||
+      inmate.currentLocationName.indexOf(this.sortOpts.filterVal) > -1;
+    }).sort((a:Inmate, b:Inmate):number=>{
       if((this.sortOpts.asc && a[this.sortOpts.prop] > b[this.sortOpts.prop]) || (!this.sortOpts.asc && a[this.sortOpts.prop] < b[this.sortOpts.prop]) ){
         return 1;
       } else if((this.sortOpts.asc && a[this.sortOpts.prop] < b[this.sortOpts.prop]) || (!this.sortOpts.asc && a[this.sortOpts.prop] > b[this.sortOpts.prop]) ){
         return -1;
       }
       return 0;
-    })
+    });
   }
 }
